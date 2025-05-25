d314049ff4cdb7fd5d444394be9b5582fda75947
thu_wang <1350989027@qq.com>
2025-05-25 15:47:36 +0800

    [AIHome]support xiaozhi ai smart home
    -Support 1602 LCD screen
    -Support SK6812 LEDs
    -Support control door/window/fan/Buzzer etc.
    
    mrwang_tju<1350989027@qq.com>

--- examples/ai_voice/ai_voice.ino
+++ examples/ai_voice/ai_voice.ino
@@ -1,3 +1,12 @@
+#include <ai_vox_engine.h>
+#include <ai_vox_observer.h>
+#include <audio_input_device.h>
+#include <audio_output_device.h>
+#include <i2s_std_audio_input_device.h>
+#include <i2s_std_audio_output_device.h>
+#include <iot_entity.h>
+#include <ESP32Servo.h>
+
 #include <WiFi.h>
 
 #include "ai_vox_engine.h"
@@ -5,44 +14,77 @@
 #include "i2s_std_audio_input_device.h"
 #include "i2s_std_audio_output_device.h"
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C mylcd(0x27,16,2);
+
+#include <FastLED.h>
+
 #ifndef WIFI_SSID
-#define WIFI_SSID "ssid"
+#define WIFI_SSID "尚奇青少年创新学院"
 #endif
 
 #ifndef WIFI_PASSWORD
-#define WIFI_PASSWORD "password"
+#define WIFI_PASSWORD "12345678"
 #endif
 
 namespace {
 #if defined(ARDUINO_ESP32_DEV)
-constexpr gpio_num_t kMicPinBclk = GPIO_NUM_25;
-constexpr gpio_num_t kMicPinWs = GPIO_NUM_26;
-constexpr gpio_num_t kMicPinDin = GPIO_NUM_27;
+constexpr gpio_num_t kMicPinBclk = GPIO_NUM_27;
+constexpr gpio_num_t kMicPinWs = GPIO_NUM_16;
+constexpr gpio_num_t kMicPinDin = GPIO_NUM_14;
 
-constexpr gpio_num_t kSpeakerPinBclk = GPIO_NUM_33;
+constexpr gpio_num_t kSpeakerPinBclk = GPIO_NUM_4;
 constexpr gpio_num_t kSpeakerPinWs = GPIO_NUM_32;
-constexpr gpio_num_t kSpeakerPinDout = GPIO_NUM_23;
+constexpr gpio_num_t kSpeakerPinDout = GPIO_NUM_2;
 
-constexpr gpio_num_t kTriggerPin = GPIO_NUM_34;
-constexpr gpio_num_t kLedPin = GPIO_NUM_2;
+constexpr gpio_num_t kTriggerPin = GPIO_NUM_25;
+constexpr gpio_num_t kLedPin = GPIO_NUM_26;
+constexpr gpio_num_t kFanPin = GPIO_NUM_18;
+constexpr gpio_num_t kWindowServoPin = GPIO_NUM_13;
+constexpr gpio_num_t kDoorServoPin = GPIO_NUM_12;
+constexpr gpio_num_t kBuzzerPin = GPIO_NUM_5;
+
+constexpr gpio_num_t kPeopleDetect = GPIO_NUM_34;
+constexpr gpio_num_t kRGB6812Pin = GPIO_NUM_17;
+
+//TodoList...
+//少气体传感器
+//NFC卡
+//蜂鸣器
+
 
 #elif defined(ARDUINO_ESP32S3_DEV)
-constexpr gpio_num_t kMicPinBclk = GPIO_NUM_5;
-constexpr gpio_num_t kMicPinWs = GPIO_NUM_2;
-constexpr gpio_num_t kMicPinDin = GPIO_NUM_4;
+constexpr gpio_num_t kMicPinBclk = GPIO_NUM_19;
+constexpr gpio_num_t kMicPinWs = GPIO_NUM_46;
+constexpr gpio_num_t kMicPinDin = GPIO_NUM_20;
 
-constexpr gpio_num_t kSpeakerPinBclk = GPIO_NUM_13;
+constexpr gpio_num_t kSpeakerPinBclk = GPIO_NUM_2;
 constexpr gpio_num_t kSpeakerPinWs = GPIO_NUM_14;
 constexpr gpio_num_t kSpeakerPinDout = GPIO_NUM_1;
 
 constexpr gpio_num_t kTriggerPin = GPIO_NUM_0;
-constexpr gpio_num_t kLedPin = GPIO_NUM_6;
+constexpr gpio_num_t kLedPin = GPIO_NUM_48;
+constexpr gpio_num_t kFanPin = GPIO_NUM_18;
+constexpr gpio_num_t kWindowServerPin = GPIO_NUM_13;
 #endif
 
 auto g_observer = std::make_shared<ai_vox::Observer>();
 std::shared_ptr<ai_vox::iot::Entity> g_led_iot_entity;
+std::shared_ptr<ai_vox::iot::Entity> g_fan_iot_entity;
+std::shared_ptr<ai_vox::iot::Entity> g_window_iot_entity;
+std::shared_ptr<ai_vox::iot::Entity> g_door_iot_entity;
+std::shared_ptr<ai_vox::iot::Entity> g_SK6812_iot_entity;
 std::shared_ptr<ai_vox::iot::Entity> g_speaker_iot_entity;
 auto g_audio_output_device = std::make_shared<ai_vox::I2sStdAudioOutputDevice>(kSpeakerPinBclk, kSpeakerPinWs, kSpeakerPinDout);
+Servo myservoWindow;
+Servo myservoDoor;
+bool SK6812State = false;
+int64_t SK6812Mode = 0;
+
+
+#define LED_COUNT 4
+CRGB leds[LED_COUNT];
 
 std::string RoleToString(const ai_vox::ChatRole role) {
   switch (role) {
@@ -136,6 +178,183 @@ void InitIot() {
 
   // 5.Register the LED entity with the AI Vox engine
   ai_vox_engine.RegisterIotEntity(g_led_iot_entity);
+
+  //fan 风扇定义
+  // 1.Define the properties for the fan entity
+  std::vector<ai_vox::iot::Property> fan_properties({
+      {
+          "state",                       // property name
+          "风扇开关状态",               // property description
+          ai_vox::iot::ValueType::kBool  // property type
+      },
+      // add more properties as needed
+  });
+
+  // 2.Define the functions for the fan entity
+  std::vector<ai_vox::iot::Function> fan_functions({
+      {"TurnOn",     // function name
+       "打开风扇",  // function description
+       {
+           // no parameters
+       }},
+      {"TurnOff",    // function name
+       "关闭风扇",  // function description
+       {
+           // no parameters
+       }},
+      // add more functions as needed
+  });
+
+  // 3.Create the fan entity
+  g_fan_iot_entity = std::make_shared<ai_vox::iot::Entity>("fan",                      // name
+                                                           "散热风扇",                    // description
+                                                           std::move(fan_properties),  // properties
+                                                           std::move(fan_functions)    // functions
+  );
+
+  // 4.Initialize the fan entity with default values
+  g_fan_iot_entity->UpdateState("state", false);
+
+  // 5.Register the fan entity with the AI Vox engine
+  ai_vox_engine.RegisterIotEntity(g_fan_iot_entity);
+
+  //智能家居窗户
+  // 1.Define the properties for the window entity
+  std::vector<ai_vox::iot::Property> window_properties({
+      {
+          "state",                       // property name
+          "窗户开关的状态",               // property description
+          ai_vox::iot::ValueType::kBool  // property type
+      },
+      // add more properties as needed
+  });
+
+  // 2.Define the functions for the window entity
+  std::vector<ai_vox::iot::Function> window_functions({
+      {"TurnOn",     // function name
+       "打开窗户",  // function description
+       {
+           // no parameters
+       }},
+      {"TurnOff",    // function name
+       "关闭窗户",  // function description
+       {
+           // no parameters
+       }},
+      // add more functions as needed
+  });
+
+  // 3.Create the window entity
+  g_window_iot_entity = std::make_shared<ai_vox::iot::Entity>("window",                      // name
+                                                           "家里的自动窗户",                    // description
+                                                           std::move(window_properties),  // properties
+                                                           std::move(window_functions)    // functions
+  );
+
+  // 4.Initialize the window entity with default values
+  g_window_iot_entity->UpdateState("state", false);
+
+  // 5.Register the window entity with the AI Vox engine
+  ai_vox_engine.RegisterIotEntity(g_window_iot_entity);
+
+  //智能家居门锁
+  // 1.Define the properties for the door entity
+  std::vector<ai_vox::iot::Property> door_properties({
+      {
+          "state",                       // property name
+          "门锁开关状态",               // property description
+          ai_vox::iot::ValueType::kBool  // property type
+      },
+      // add more properties as needed
+  });
+
+  // 2.Define the functions for the window entity
+  std::vector<ai_vox::iot::Function> door_functions({
+      {"TurnOn",     // function name
+       "打开门锁",  // function description
+       {
+           // no parameters
+       }},
+      {"TurnOff",    // function name
+       "关闭门锁",  // function description
+       {
+           // no parameters
+       }},
+      // add more functions as needed
+  });
+
+  // 3.Create the door entity
+  g_door_iot_entity = std::make_shared<ai_vox::iot::Entity>("door",                      // name
+                                                           "家里的门锁",                    // description
+                                                           std::move(door_properties),  // properties
+                                                           std::move(door_functions)    // functions
+  );
+
+  // 4.Initialize the door entity with default values
+  g_door_iot_entity->UpdateState("state", false);
+
+  // 5.Register the door entity with the AI Vox engine
+  ai_vox_engine.RegisterIotEntity(g_door_iot_entity);
+
+  //娱乐灯光系统
+  // 1.Define the properties for the door entity
+  std::vector<ai_vox::iot::Property> SK6812_properties({
+      {
+          "mode",                           // property name
+          "客厅的霓虹灯的模式",               // property description
+          ai_vox::iot::ValueType::kNumber   // property type
+      },
+      {
+          "state",                       // property name
+          "霓虹灯开关状态",               // property description
+          ai_vox::iot::ValueType::kBool  // property type
+      },
+      // add more properties as needed
+  });
+
+  // 2.Define the functions for the SK6812 entity
+  std::vector<ai_vox::iot::Function> SK6812_functions({
+      {"SetMode",  // function name
+       "设置霓虹灯模式",   // function description
+       {
+          {
+              "mode",                         // parameter name
+              "整数0或1",                     // parameter description
+              ai_vox::iot::ValueType::kNumber,  // parameter type
+              true                              // parameter required
+          },
+          // add more parameters as needed
+       }},
+      {"TurnOff",  // function name
+       "关闭霓虹灯",   // function description
+       {
+        {
+        },
+        // add more parameters as needed
+       }},
+      {"TurnOn",  // function name
+       "打开霓虹灯",   // function description
+       {
+        {
+        },
+        // add more parameters as needed
+       }},
+      // add more functions as needed
+  });
+
+  // 3.Create the SK6812 entity
+  g_SK6812_iot_entity = std::make_shared<ai_vox::iot::Entity>("SK6812",                      // name
+                                                           "客厅的霓虹灯，用于放松、招待客人", // description
+                                                           std::move(SK6812_properties),  // properties
+                                                           std::move(SK6812_functions)    // functions
+  );
+
+  // 4.Initialize the SK6812 entity with default values
+  g_SK6812_iot_entity->UpdateState("mode", 0);
+  g_SK6812_iot_entity->UpdateState("state", false);
+
+  // 5.Register the SK6812 entity with the AI Vox engine
+  ai_vox_engine.RegisterIotEntity(g_SK6812_iot_entity);
 }
 
 void PrintMemInfo() {
@@ -201,6 +420,30 @@ void setup() {
 
   pinMode(kLedPin, OUTPUT);
   digitalWrite(kLedPin, LOW);
+
+  pinMode(kFanPin, OUTPUT);
+  digitalWrite(kFanPin, LOW);
+
+  myservoWindow.attach(kWindowServoPin, 500, 2500);
+  myservoWindow.write(0);
+
+  myservoDoor.attach(kDoorServoPin, 500, 2500);
+  myservoDoor.write(0);
+
+  pinMode(kBuzzerPin, OUTPUT);
+  digitalWrite(kBuzzerPin, LOW);
+
+  mylcd.init();
+  mylcd.backlight();
+  mylcd.clear();
+
+  mylcd.setCursor(0, 0);
+  mylcd.print("shangqi AI+ home");
+
+  pinMode(kPeopleDetect, INPUT);
+
+  FastLED.addLeds<SK6812, kRGB6812Pin, GRB>(leds, LED_COUNT);
+
   InitIot();
 
   auto audio_input_device = std::make_shared<ai_vox::I2sStdAudioInputDevice>(kMicPinBclk, kMicPinWs, kMicPinDin);
@@ -230,26 +473,38 @@ void loop() {
       switch (state_changed_event->new_state) {
         case ai_vox::ChatState::kIdle: {
           printf("Idle\n");
+          mylcd.setCursor(0, 1);
+          mylcd.print("Idle");
           break;
         }
         case ai_vox::ChatState::kIniting: {
           printf("Initing...\n");
+          mylcd.setCursor(0, 1);
+          mylcd.print("Initing...");
           break;
         }
         case ai_vox::ChatState::kStandby: {
           printf("Standby\n");
+          mylcd.setCursor(0, 1);
+          mylcd.print("Standby");
           break;
         }
         case ai_vox::ChatState::kConnecting: {
           printf("Connecting...\n");
+          mylcd.setCursor(0, 1);
+          mylcd.print("Connecting...\n");
           break;
         }
         case ai_vox::ChatState::kListening: {
           printf("Listening...\n");
+          mylcd.setCursor(0, 1);
+          mylcd.print("Listening...");
           break;
         }
         case ai_vox::ChatState::kSpeaking: {
           printf("Speaking...\n");
+          mylcd.setCursor(0, 1);
+          mylcd.print("Speaking...");
           break;
         }
         default: {
@@ -303,8 +558,112 @@ void loop() {
           }
         }
       }
+      else if (iot_message_event->name == "fan") {
+        if (iot_message_event->function == "TurnOn") {
+          printf("turn on fan\n");
+          digitalWrite(kFanPin, HIGH);
+          g_fan_iot_entity->UpdateState("state", true);  // Note: Must UpdateState after change the device state
+        } else if (iot_message_event->function == "TurnOff") {
+          printf("turn off fan\n");
+          digitalWrite(kFanPin, LOW);
+          g_fan_iot_entity->UpdateState("state", false); // Note: Must UpdateState after change the device state
+        }
+      }
+      else if (iot_message_event->name == "window") {
+        if (iot_message_event->function == "TurnOn") {
+          printf("turn on window\n");
+          myservoWindow.write(100);
+          g_window_iot_entity->UpdateState("state", true);  // Note: Must UpdateState after change the device state
+        } else if (iot_message_event->function == "TurnOff") {
+          printf("turn off window\n");
+          myservoWindow.write(0);
+          g_window_iot_entity->UpdateState("state", false); // Note: Must UpdateState after change the device state
+        }
+      }
+      else if (iot_message_event->name == "door") {
+        if (iot_message_event->function == "TurnOn") {
+          printf("turn on door\n");
+          myservoDoor.write(90);
+          g_door_iot_entity->UpdateState("state", true);  // Note: Must UpdateState after change the device state
+        } else if (iot_message_event->function == "TurnOff") {
+          printf("turn off door\n");
+          myservoDoor.write(0);
+          g_door_iot_entity->UpdateState("state", false); // Note: Must UpdateState after change the device state
+        }
+      }
+      else if (iot_message_event->name == "SK6812") {
+        if (iot_message_event->function == "SetMode") {
+          if (const auto it = iot_message_event->parameters.find("mode"); it != iot_message_event->parameters.end()) {
+            auto mode = it->second;
+            if (std::get_if<int64_t>(&mode)) {
+              printf("SK6812 mode: %lld\n", std::get<int64_t>(mode));
+              SK6812State = true;
+              SK6812Mode = std::get<int64_t>(mode);
+              g_SK6812_iot_entity->UpdateState("mode", std::get<int64_t>(mode)); // Note: Must UpdateState after change the device state
+              g_SK6812_iot_entity->UpdateState("state", true);
+            }
+          }
+        }
+        if (iot_message_event->function == "TurnOff") {
+            SK6812State = false;
+            g_SK6812_iot_entity->UpdateState("state", false);
+        }
+        if (iot_message_event->function == "TurnOn") {
+            SK6812State = true;
+            g_SK6812_iot_entity->UpdateState("state", true);
+        }
+      }
     }
   }
 
+  //传感器自动化场景
+  iotAutoTask();
+
   taskYIELD();
+}
+
+void iotAutoTask()
+{
+  //人体传感器状态，有问题报警
+  boolean peopleDetectVal = digitalRead(kPeopleDetect);
+  // printf("peopleDetectVal %d\n", peopleDetectVal);
+
+  //客厅霓虹灯光控制
+  if (SK6812State)
+  {
+    if (SK6812Mode == 0)
+    {
+      rainbowEffect();
+    }
+    else if(SK6812Mode == 1)
+    {
+      meteorShower();
+    }
+  }
+  else
+  {
+    fill_solid(leds, LED_COUNT, CRGB::Black);  // 参数：LED数组、数量、颜色
+    FastLED.show();
+  }
+}
+
+void rainbowEffect() {
+  static uint8_t hue = 0; // 色调值（0-255）
+  for(int i=0; i<LED_COUNT; i++) {
+    leds[i] = CHSV(hue + (i*2), 255, 255); // 创建彩虹色带
+  }
+  FastLED.show();
+  hue += 1;  // 改变色调值产生动态效果
+  delay(20);
+}
+
+void meteorShower() {
+  fadeToBlackBy(leds, LED_COUNT, 50); // 渐隐效果
+
+  static int pos = 0;
+  leds[pos] = CHSV(random8(), 255, 255); // 随机颜色
+  pos = (pos + 1) % LED_COUNT;
+
+  FastLED.show();
+  delay(20);
 }
