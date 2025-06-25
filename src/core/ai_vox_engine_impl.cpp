11df1a4cf35fe2e36f8fe0a25534c7d1c8bb4810
arex.huang <arex@null-lab.com>
2025-06-25 17:48:51 +0800

    支持PlatformIO编译环境

--- src/core/ai_vox_engine_impl.cpp
+++ src/core/ai_vox_engine_impl.cpp
@@ -1,28 +1,17 @@
 #include "ai_vox_engine_impl.h"
 
-#ifdef ARDUINO
-#include "espressif_button/button_gpio.h"
-#include "espressif_button/iot_button.h"
-#else
-#include <button_gpio.h>
-#include <esp_lvgl_port.h>
-#include <iot_button.h>
-#endif
-
 #include <cJSON.h>
-#include <driver/i2c_master.h>
 #include <esp_crt_bundle.h>
-#include <esp_lcd_panel_io.h>
-#include <esp_lcd_panel_ops.h>
-#include <esp_lcd_panel_vendor.h>
 #include <esp_mac.h>
+#include <esp_timer.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-#include <mqtt_client.h>
 
 #include "ai_vox_observer.h"
 #include "audio_input_engine.h"
 #include "audio_output_engine.h"
+#include "espressif_button/button_gpio.h"
+#include "espressif_button/iot_button.h"
 #include "fetch_config.h"
 
 #ifndef CLOGGER_SEVERITY
